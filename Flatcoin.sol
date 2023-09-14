58941281ae04e77b2450e717f9460fb6b9c1df6c
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-09-13 14:54:58 -1000

    Update Flatcoin.sol

--- Flatcoin.sol
+++ Flatcoin.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol"; // Import Chainlink AggregatorV3Interface
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
-contract Flatcoin is ERC20, Ownable {
+contract Flatcoin is ERC20, Ownable, Pausable, VRFConsumerBase {
     uint256 public collateralRatio; // Ratio of collateral required for minting (e.g., 200%)
     uint256 public totalCollateral; // Total collateral locked in the contract
     uint256 public totalSupply; // Total Flatcoins in circulation
@@ -21,14 +22,22 @@ contract Flatcoin is ERC20, Ownable {
 
     AggregatorV3Interface internal priceFeed; // Chainlink price feed interface
 
+    uint256 public interestRate; // Annual interest rate for collateral
+    mapping(address => uint256) public collateralBalances;
+    
+    bytes32 internal requestID;
+
     constructor(
         uint256 _initialSupply,
         uint256 _initialCollateralRatio,
-        address _priceFeedAddress
-    ) ERC20("Flatcoin", "FLAT") {
+        address _priceFeedAddress,
+        address _vrfCoordinator,
+        address _link
+    ) ERC20("Flatcoin", "FLAT") VRFConsumerBase(_vrfCoordinator, _link) {
         _mint(msg.sender, _initialSupply * (10**uint256(decimals())));
         collateralRatio = _initialCollateralRatio;
         priceFeed = AggregatorV3Interface(_priceFeedAddress);
+        interestRate = 5; // 5% annual interest rate (can be adjusted)
     }
 
     // Update the price feed address if needed
@@ -37,7 +46,7 @@ contract Flatcoin is ERC20, Ownable {
     }
 
     // Mint Flatcoins by locking up collateral
-    function mint(uint256 _amount) external payable {
+    function mint(uint256 _amount) external payable whenNotPaused {
         uint256 requiredCollateral = (_amount * collateralRatio) / (10**uint256(decimals()));
         require(msg.value == requiredCollateral, "Incorrect collateral amount");
 
@@ -47,7 +56,7 @@ contract Flatcoin is ERC20, Ownable {
     }
 
     // Redeem Flatcoins for collateral
-    function redeem(uint256 _amount) external {
+    function redeem(uint256 _amount) external whenNotPaused {
         require(balanceOf(msg.sender) >= _amount, "Insufficient Flatcoins");
         require(totalSupply >= _amount, "Not enough Flatcoins in circulation");
 
@@ -91,8 +100,42 @@ contract Flatcoin is ERC20, Ownable {
         costOfLiving = newCostOfLiving;
     }
 
-    // Fallback function to accept collateral
-    receive() external payable {}
+    // Pause and unpause the contract in case of emergency
+    function pause() external onlyOwner {
+        _pause();
+    }
 
-    // Implement governance features, interest rates, and other functionalities as needed
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    // Implement interest calculations for collateral
+    function calculateInterest(address _user) internal view returns (uint256) {
+        uint256 elapsedTime = block.timestamp - collateralBalances[_user];
+        return (collateralBalances[_user] * interestRate * elapsedTime) / (365 days * 100);
+    }
+
+    // Deposit and withdraw collateral with interest
+    function depositCollateral() external payable {
+        collateralBalances[msg.sender] += msg.value;
+    }
+
+    function withdrawCollateral(uint256 _amount) external {
+        require(collateralBalances[msg.sender] >= _amount, "Insufficient collateral");
+        uint256 interest = calculateInterest(msg.sender);
+        collateralBalances[msg.sender] -= _amount;
+        payable(msg.sender).transfer(_amount + interest);
+    }
+
+    // Chainlink VRF related functions
+    function requestRandomNumber() external onlyOwner returns (bytes32) {
+        require(LINK.balanceOf(address(this)) >= chainlinkFee, "Not enough LINK tokens");
+        requestID = requestRandomness(keyHash, chainlinkFee);
+        return requestID;
+    }
+
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomNumber) internal override {
+        // Use the random number as needed in your contract
+        // For example, you can implement random events or decisions
+    }
 }
